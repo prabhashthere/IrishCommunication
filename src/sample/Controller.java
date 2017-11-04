16a2b866af699c8c14fc112a24d8d79a3687f572
calmnirmal1995 <32019089+calmnirmal1995@users.noreply.github.com>
2017-11-04 20:37:39 +0530

    da

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    public void pushButton(javafx.event.ActionEvent event){
+        System.out.println("Hello Hagga");
+
+    }
+}
